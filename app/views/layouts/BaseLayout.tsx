4e68bde545d3b9ba5ef5c14a754882f58d2e3001
Raphael Kabo <mail@raphaelkabo.com>
2024-12-01 23:05:15 +0000

    Add favicon

--- app/views/layouts/BaseLayout.tsx
+++ app/views/layouts/BaseLayout.tsx
@@ -18,6 +18,7 @@ export function BaseLayout(
                     name="viewport"
                     content="width=device-width, initial-scale=1.0"
                 />
+                <link rel="icon" href="/static/favicon.png" />
                 <script src="https://unpkg.com/htmx.org@2.0.3"></script>
                 <script src="https://unpkg.com/htmx.org@1.9.12/dist/ext/ws.js">
                 </script>
